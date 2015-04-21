0aa57ca9ac713437baaae06197bd8fbec5598c21
曹祖鹏 <chinalibra@163.com>
2015-04-21 11:30:54 +0800

    modify version，fix lowcase filename in setup.py

--- tests/test_rpclib.py
+++ tests/test_rpclib.py
@@ -11,12 +11,13 @@ if __name__ == '__main__':
     config = ApplicationConfig('test_rpclib')
     service_interface = 'com.ofpay.demo.api.UserProvider'
     # 该对象较重，有zookeeper的连接，需要保存使用
-    # registry = ZookeeperRegistry('192.168.59.103:2181', config)
-    registry = MulticastRegistry('224.5.6.7:1234', config)
+    registry = ZookeeperRegistry('192.168.59.103:2181', config)
+    # registry = MulticastRegistry('224.5.6.7:1234', config)
     user_provider = DubboClient(service_interface, registry, version='2.0')
     for i in range(1000):
         try:
             print user_provider.getUser('A003')
+            print user_provider.getUser(123)
             print user_provider.queryUser(
                 {u'age': 18, u'time': 1428463514153, u'sex': u'MAN', u'id': u'A003', u'name': u'zhangsan'})
             datas = user_provider.queryAll()
